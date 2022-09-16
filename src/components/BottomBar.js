04f4bacf3b3a10258e6248e54c1dc52822a1a346
yse92 <yarik49best@gmail.com>
2022-09-16 10:03:22 +0300

    prop bug fixed

--- src/components/BottomBar.js
+++ src/components/BottomBar.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {View, Text, TouchableOpacity} from 'react-native'
 import {styles} from '../styles/BottomBar.styles';
 
-export const BottomBar = (text) => {
+export const BottomBar = ({text}) => {
     return (
       <View style={styles.container}>
         <TouchableOpacity>
